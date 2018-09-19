8a48cbb45dd9d475612174b5f67e36348bc475a3
JosueRodriguez <josue.a.rodriguez@ttu.edu>
2018-09-18 19:47:23 -0500

    beginning the object segmentation

--- PiFiles/FirstConnect.py
+++ PiFiles/FirstConnect.py
@@ -1,10 +1,16 @@
-import requests
+input("Pause.")
+
 import json
+import requests
 
+print("Getting Data...\n")
 r = requests.get('http://172.16.0.1:8001/FieldData/GetData')
+print(r.text)
+print('\n')
+print("Got Data.\n")
 
-print(r.json())
+parsed = json.loads(r.text)
+ball = parsed['Ball']
+print(ball['Object Center'])
 
-parsed = json.loads(r)
-
-
+input("Function Complete. Press 'Enter' to exit.")
